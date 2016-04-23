c077bdaa835fc50e755ab17860eca2e9ece2d228
funkydorian <derya.sezen@gmail.com>
2016-04-23 22:23:45 +0300

    fix syntax

--- synthetics_parsehtml01.js
+++ synthetics_parsehtml01.js
@@ -1,10 +1,10 @@
 var assert = require('assert');
 
-$browser.get('https://www.domain.com).then(function(){
+$browser.get('https://www.domain.com').then(function(){
   return $browser.findElement($driver.By.className('search-list-container')).then(function(element){
     return element.findElement($driver.By.tagName('li')).then(function(element){
      	return element.getAttribute('att01').then(function(text){
-        	assert.equal("text2beequal", text, not found"");
+        	assert.equal("text2beequal", text, "not found");
       });
     });
   });
